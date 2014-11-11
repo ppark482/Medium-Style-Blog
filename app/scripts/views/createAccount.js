42aa9be7b1f93c7e76ac9b143960ae63e4af0562
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-11 16:12:00 -0500

    changes

--- app/scripts/views/createAccount.js
+++ app/scripts/views/createAccount.js
@@ -1,23 +1,23 @@
 (function() {
 
   App.Views.CreateAcct = Parse.View.extend({
-    el: '.middleRegion',
+    el: '#middleRegion',
 
     event: {},
 
-    // template: _.template($('#acctTemp').html()),
 
     initialize: function() {
       this.render;
+
+    
     },
 
     render: function(){
-      //  this.$el.empty();
-      //  this.$el.html(
 
-      var acct = $('#acctTemp').html();
-      var renderAcct = _.template(acct);
-      this.$el.html(acct);
+
+
+
+
     }
 
 
