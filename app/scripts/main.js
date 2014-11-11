dcac628aab975ec3b02779c1853aaec02817c41f
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-11 16:01:36 -0500

    not rendering

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,13 @@
 
-Parse.initialize("b9ihleuYJm7Z20BGiIeVfE3XHmgNZoGP0P6tWs7A", "c3b6HWtmmYpCntKDKl85BZAt1VLUvHqeXTEVnkEk");console.log('works');
+Parse.initialize("b9ihleuYJm7Z20BGiIeVfE3XHmgNZoGP0P6tWs7A", "c3b6HWtmmYpCntKDKl85BZAt1VLUvHqeXTEVnkEk");
 
-(function (
+(function() {
+
+  $('.signUp').on('click', function(){
+    new App.Views.CreateAcct();
+    console.log('click');
+
+  });
 
 
-) {}());
+}());
--- app/scripts/views/createAccount.js
+++ app/scripts/views/createAccount.js
@@ -1,4 +1,26 @@
-(function (
+(function() {
+
+  App.Views.CreateAcct = Parse.View.extend({
+    el: '.middleRegion',
+
+    event: {},
+
+    // template: _.template($('#acctTemp').html()),
+
+    initialize: function() {
+      this.render;
+    },
+
+    render: function(){
+      //  this.$el.empty();
+      //  this.$el.html(
+
+      var acct = $('#acctTemp').html();
+      var renderAcct = _.template(acct);
+      this.$el.html(acct);
+    }
 
 
-) {}());
+  });
+
+}());
