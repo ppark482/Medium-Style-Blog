456df3b3f3f7a17b4fd94e764971f18c5b55be5d
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-11 14:36:18 -0500

    linked up js files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,2 @@
-console.log('The Iron Yard Rocks');
+Parse.initialize("b9ihleuYJm7Z20BGiIeVfE3XHmgNZoGP0P6tWs7A", "c3b6HWtmmYpCntKDKl85BZAt1VLUvHqeXTEVnkEk");console.log('works');
+console.log('works');
