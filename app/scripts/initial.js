d1ddf047e97f5e362b65582e94dcc3b0f96dbce1
Paul Park <ppark482@gmail.com>
2014-11-11 14:25:34 -0500

    added script files

---
+++ app/scripts/initial.js
@@ -0,0 +1,5 @@
+window.App = {};
+        App.Models = {};
+        App.Collections = {};
+        App.Views = {};
+        App.Routers = {};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('The Iron Yard Rocks');
+Parse.initialize("b9ihleuYJm7Z20BGiIeVfE3XHmgNZoGP0P6tWs7A", "c3b6HWtmmYpCntKDKl85BZAt1VLUvHqeXTEVnkEk");
+
+(function() {
+
+
+
+}()); // end of IIF
