0fd42f8b13fc85e712ed89764c95b2eae80bbcf0
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-14 14:09:39 -0500

    log stat

--- app/scripts/views/navBar.js
+++ app/scripts/views/navBar.js
@@ -21,11 +21,20 @@
     render            : function () {
       // Renders Nav Bar
       this.$el.html(this.template);
+      this.logstat();
+      // $('.guest').append()
     }, // end of render
 
-    home        : function() {
+    home              : function() {
 
       new App.Views.Home({collection: App.posts, user: App.user});
+    },
+
+    logstat             : function() {
+
+      var a = App.user.getUsername();
+      $('.guest').append(" "+ a + " ");
+
     },
 
     signupForm        : function() {
