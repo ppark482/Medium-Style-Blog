e5e29d97cae6d6eeee2ff9acd9cf1b3bedd02bb5
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-14 12:05:34 -0500

    edit

--- app/scripts/views/singlePost.js
+++ app/scripts/views/singlePost.js
@@ -13,12 +13,24 @@
     initialize        : function(options) {
       this.options=options;
       this.render();
+      this.edit();
     }, // end of initialize
 
     render            : function(options) {
       new App.Views.NavBar();
+      new App.Views.FooterAuthors();
       this.$el.html(this.template(this.options.model.toJSON()));
-    } // end of render
+    }, // end of render
+
+    edit              : function(user) {
+
+      if(App.user ===user) {
+        console.log(user);
+        console.log("yes");
+        $('.ifAuthor').append('<button class="editBtn">Edit</button>');
+      }
+    }
+
 
 
 
