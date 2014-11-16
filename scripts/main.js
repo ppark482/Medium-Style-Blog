ec7af766d2cddab50fbe59528b4eea7f707a5cb8
Andrew El-Masry <andrewelmasry@gmail.com>
2014-11-16 17:42:01 -0500

    logo

--- scripts/main.js
+++ scripts/main.js
@@ -280,11 +280,6 @@ window.App = {};
     render: function (options) {
       var self = this;
       var posts = App.posts.models;
-      console.log(posts);
-      // var published = posts.findWhere({ status : 'published' });
-      // paul working here ++++++++++++++++++++++++++++++++++++++++++++++++++
-      // console.log(published);
-      // console.log(App.user_collection.models);
       _.each(posts, function(x) {
         self.$el.prepend(self.template(x.toJSON()));
       });
@@ -324,8 +319,6 @@ window.App = {};
     render            : function(options) {
       var self = this;
       var posts = this.options.model;
-      console.log(posts);
-      console.log(options);
       // Need to render selected author's name
       // Pull id from clicked name from footer bar
       self.$el.html(self.template(posts.toJSON()));
@@ -365,18 +358,17 @@ window.App = {};
       // console.log(App.posts.models[0].attributes.user.id);
       // Query parse to find posts
       // for the passed user
-      console.log(options.model);
+      console.log(options.model.attributes.username);
       var query = new Parse.Query(App.Models.Post);
-      query.equalTo('user', options.model);
+      query.equalTo('author', options.model.attributes.username);
       query.find({
         success: function(results) {
           _.each(results, function(x) {
-            // console.log(x.toJSON());
             self.$el.append(self.template(x.toJSON()));
           });
         }
-      }); // end of query find
-    } // end of postQuery
+      });
+    } // end of query find
 
   }); // end of view
 
@@ -637,16 +629,19 @@ window.App = {};
       // Retrieves passed in parameter from router
       this.options = options;
       App.posts.on('sync', this, this);
-      this.render();
+      this.render(options);
     }, // end of initialize
 
-    render                  : function () {
+    render                  : function (options) {
       // Renders Nav Bar
       new App.Views.NavBar();
       // Renders Footer
       new App.Views.Footer();
+      new App.Views.FooterAuthors();
       // Render author page onto page
       this.$el.html(this.template(this.options.toJSON()));
+      // Render this Author's Post view
+      new App.Views.MyAcctPosts(options);
     },
 
     addPost                 : function () {
@@ -654,6 +649,57 @@ window.App = {};
       App.router.navigate('addpost', { trigger: true});
     } // end of addPost
 
+
+  }); // end of view
+
+
+}()); // end of IIF
+
+(function () {
+
+  App.Views.MyAcctPosts = Parse.View.extend({
+
+    tagName                 : 'ul',
+    className               : 'yourPosts',
+
+    events: {
+    }, // end of events
+
+    template                : _.template($('#myAcctPostsTemp').html()),
+
+    initialize              : function (user) {
+      // Retrieves passed in parameter from router
+      App.posts.on('sync', this, this);
+      this.render();
+      // Get our element on the page
+      this.postQuery(user);
+      $('#yourPosts').html(this.$el);
+    }, // end of initialize
+
+    render                  : function () {
+      var self = this;
+      var posts = App.posts.models;
+      _.each(App.posts.models, function (x) {
+        self.$el.prepend(self.template(x.toJSON()));
+      });
+    },
+
+    postQuery               : function (user) {
+      var self = this;
+      self.$el.empty();
+      var username = user.attributes.username;
+      console.log(username);
+      query = new Parse.Query(App.Models.Post);
+      query.equalTo('author', username);
+      query.find({
+        success: function (results) {
+          console.log(results);
+          _.each(results, function (x) {
+            self.$el.append(self.template(x.toJSON()));
+          });
+        }
+      });
+    } // end of query
 
   }); // end of view
 
